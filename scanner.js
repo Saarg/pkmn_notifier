deb2e6752be1912479c6a55fe055709ff767aa94
BuildTools <milsonneau.jean@free.fr>
2016-11-09 22:47:11 +0100

    lower scan rate since we are on a static point no need to add useless load

--- scanner.js
+++ scanner.js
@@ -13,73 +13,39 @@ var client = new pogobuf.Client()
 
 var nearbyList = []
 
-var start_scanning = function(callback) {
-  console.log(this.username)
-  let self = this
-  // Login ===================================================================
+var scan = function(self, callback) {
   google.login(self.username, self.password).then(token => {
     client.setAuthInfo('google', token)
     client.setPosition(self.lat, self.lng)
     return client.init()
   }).then(() => {
-    console.log('Authenticated, waiting for first map refresh (30s)')
-    notifier.notify({
-      icon: path.join(__dirname, 'icons/app.png'),
-      title: 'Authenticated ' + self.lat + ' ' + self.lng,
-      message: 'Waiting for first map refresh (30s)',
-      urgency: 'critical'
-    })
+    var cellIDs = pogobuf.Utils.getCellIDs(self.lat, self.lng)
+    return bluebird.resolve(client.getMapObjects(cellIDs, Array(cellIDs.length).fill(0))).then(mapObjects => {
+      return mapObjects.map_cells
+    }).each(cell => {
+      return bluebird.resolve(cell.catchable_pokemons).each(catchablePokemon => {
+        if (nearbyList.indexOf(catchablePokemon.pokemon_id) == -1) {
+          console.log('main.js - There is a ' + pogobuf.Utils.getEnumKeyByValue(POGOProtos.Enums.PokemonId, catchablePokemon.pokemon_id) + ' near.')
+          var icon =  '' + catchablePokemon.pokemon_id
+          var pad = '000'
 
-    setInterval(() => {
-      var cellIDs = pogobuf.Utils.getCellIDs(self.lat, self.lng)
-      return bluebird.resolve(client.getMapObjects(cellIDs, Array(cellIDs.length).fill(0))).catch(reason => {
-        if (~reason.toString().indexOf('code 102')){
-          console.log('status code 102, relogging...');
-
-          google = new pogobuf.GoogleLogin()
-          client = new pogobuf.Client()
-
-          // Relogin
-          google.login(self.username, self.password).then(token => {
-            client.setAuthInfo('google', token)
-            client.setPosition(self.lat, self.lng)
-            client.init()
-          }).then(() => {
-            console.log('relogged!')
+          notifier.notify({
+            icon: path.join(__dirname, 'icons', pad.substring(0, pad.length - icon.length) + icon + ".png"),
+            title: 'Nearby pokemon',
+            message: 'There is a ' + pogobuf.Utils.getEnumKeyByValue(POGOProtos.Enums.PokemonId, catchablePokemon.pokemon_id) + ' near.',
+            urgency: 'critical'
           })
-        }
-        throw reason;
-      }).then(mapObjects => {
-        return mapObjects.map_cells
-      }).each(cell => {
-        // console.log('Cell ' + cell.s2_cell_id.toString())
-        // console.log('Has ' + cell.catchable_pokemons.length + ' catchable Pokemon')
-        return bluebird.resolve(cell.catchable_pokemons).each(catchablePokemon => {
-          // console.log(' - A ' + pogobuf.Utils.getEnumKeyByValue(POGOProtos.Enums.PokemonId, catchablePokemon.pokemon_id) + ' is asking you to catch it.')
 
-          if (nearbyList.indexOf(catchablePokemon.pokemon_id) == -1) {
-            console.log('main.js - There is a ' + pogobuf.Utils.getEnumKeyByValue(POGOProtos.Enums.PokemonId, catchablePokemon.pokemon_id) + ' near.')
-            var icon =  '' + catchablePokemon.pokemon_id
-            var pad = '000'
-            // console.log(path.join(__dirname, 'icons', pad.substring(0, pad.length - icon.length) + icon + ".png"))
-            notifier.notify({
-              icon: path.join(__dirname, 'icons', pad.substring(0, pad.length - icon.length) + icon + ".png"),
-              title: 'Nearby pokemon',
-              message: 'There is a ' + pogobuf.Utils.getEnumKeyByValue(POGOProtos.Enums.PokemonId, catchablePokemon.pokemon_id) + ' near.',
-              urgency: 'critical'
-            })
+          nearbyList.push(catchablePokemon.pokemon_id)
 
-            nearbyList.push(catchablePokemon.pokemon_id)
-
+          callback(nearbyList)
+          setTimeout(function () {
+            nearbyList.splice(nearbyList.indexOf(catchablePokemon.pokemon_id), 1)
             callback(nearbyList)
-            setTimeout(function () {
-              nearbyList.splice(nearbyList.indexOf(catchablePokemon.pokemon_id), 1)
-              callback(nearbyList)
-            }, 20 * 60000)
-          }
-        })
+          }, 20 * 60000)
+        }
       })
-    }, 30 * 1000)
+    })
   }).catch(function(err) {
     console.error(err)
     notifier.notify({
@@ -89,6 +55,15 @@ var start_scanning = function(callback) {
       urgency: 'critical'
     })
   })
+}
+
+var start_scanning = function(callback) {
+  console.log(this.username)
+  let self = this
+  scan(self, callback)
+  setInterval(() => {
+    scan(self, callback)
+  }, 10 * 60000)// scan every 10min
 }
 
 module.exports = {
